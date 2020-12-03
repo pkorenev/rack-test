62e0d0fd65c152fb8f0f4c6aeedd5c21c3a90404
pkorenev <p.korenev@voroninstudio.eu>
2020-12-03 17:09:37 +0200

    working router

---
+++ application.rb
@@ -0,0 +1,49 @@
+class Application
+  HTTP_METHOD_NAMES = %w(get post put patch delete)
+
+  def self.application
+    @application ||= Application.new
+  end
+
+  def initialize
+    @routes = []
+  end
+
+  HTTP_METHOD_NAMES.each do |method_name|
+    define_method method_name do |url, &block|
+      @routes << {url: url, method: method_name.upcase, action: block}
+    end
+  end
+
+  def call(env)
+    handle_request(env['PATH_INFO'], env['REQUEST_METHOD'])
+  end
+
+  private
+
+  def handle_request(url, method)
+    puts "method: #{method}"
+    if route = find_route(url, method)
+      response_body = route[:action].call || ''
+      status  = 200
+      headers = { "Content-Type" => "text/html" }
+      body    = [response_body]
+    else
+      status  = 404
+      headers = { "Content-Type" => "text/html" }
+      body    = ["Page not found"]
+    end
+
+    [status, headers, body]
+  end
+
+  def find_route(url, method)
+    @routes.find { |route| route[:url] == url && route[:method] == method }
+  end
+end
+
+Application::HTTP_METHOD_NAMES.each do |method_name|
+  define_method method_name do |url, &block|
+    Application.application.send(method_name, url, &block)
+  end
+end
---
+++ router.rb
@@ -0,0 +1,11 @@
+get '/frank-says' do
+  'I am GET'
+end
+
+post '/frank-says' do
+  'I am POST'
+end
+
+put '/test' do
+  'I am test PUT'
+end
